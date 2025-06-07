c4a20ed51b32f29f76a67b41111f33fd335a0e66
salgado2004 <leosalgado2004@gmail.com>
2025-06-06 22:48:28 -0300

    initial project setup with Spring Boot, MongoDB configuration, and Maven wrapper

---
+++ src/main/kotlin/br/ufpr/tads/daily_iu_services/DailyIuServicesApplication.kt
@@ -0,0 +1,11 @@
+package br.ufpr.tads.daily_iu_services
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DailyIuServicesApplication
+
+fun main(args: Array<String>) {
+	runApplication<DailyIuServicesApplication>(*args)
+}
---
+++ src/test/kotlin/br/ufpr/tads/daily_iu_services/DailyIuServicesApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.ufpr.tads.daily_iu_services
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DailyIuServicesApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
